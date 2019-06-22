651914cb6c728d9426a296099bb4533e9c925832
Higor Ferreira <hfashigor@hotmail.com>
2019-06-22 11:00:44 -0300

    Last Adjust

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <mpi.h>
 
+#include <stdio.h>
 #include "graph.h"
 
 bool getServos(vector<int> *servos, int size){
@@ -18,6 +19,12 @@ bool isServosEmpty(vector<int> *servos){
     return servos->size() == 0;
 }
 
+int popServo(vector<int> *servos){
+    int ret = servos->at(servos->size()-1);
+    servos->pop_back();
+    return ret;
+}
+
 int main()
 {
     //Variables to store rank and the number of processes
@@ -25,6 +32,7 @@ int main()
 
     //Stack to store servo processes
     vector<int> *servos = new vector<int>;
+    vector<int> *servosStack = new vector<int>;
 
     //Starting MPI (Message Passing Interface)
     MPI_Init(NULL, NULL);
@@ -36,6 +44,7 @@ int main()
 
     //Getting servos
     getServos(servos, size);
+    getServos(servosStack, size);
 
     if(rank == 0){
         //Creating Graph
@@ -54,6 +63,96 @@ int main()
         }
 
         while (true) {
+            int u, v;
+
+            while (u==v) {
+                u = g->randd();
+                v = g->randd();
+            }
+
+            if(hasServo(servosStack)){
+                //Mounting Request for short way
+                char *data = new char('C');
+                *(unsigned short*)(data+1) = (unsigned short)u;
+                *(unsigned short*)(data+3) = (unsigned short)v;
+
+                //Sending request
+                MPI_Send(data, 5, MPI_BYTE, popServo(servosStack), 0, MPI_COMM_WORLD);
+            }
+
+            void *data;
+
+            for (int i = 1; i < servos->size()+1; i++) {
+                //Wait for response from some servo
+                MPI_Recv(data, 0x1BFFF, MPI_BYTE, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+                char *dataPointer = (char*)data;
+
+                int responseRank = *(unsigned short*)dataPointer;
+                dataPointer += 2;
+                char responseType = *(char*)dataPointer;
+                dataPointer++;
+                int bufferSize = *(int*)dataPointer;
+                dataPointer += 4;
+
+                int cost = *(int*)dataPointer; dataPointer += 4;
+                int waySize = *(int*)dataPointer; dataPointer += 4;
+
+                char *way = new char[waySize*3+1];
+                char *wayPointer = way;
+
+                //Releasing unused space on memory
+                char *toErase = (char*)data+bufferSize;
+                delete toErase;
+
+                if(responseRank == i){
+
+                    servosStack->push_back(i);
+
+                    //Taking way string to show
+                    if(responseType == 'C'){
+
+                        for (int i = 0; i < waySize; i++) {
+
+                            *way = (char)*(unsigned short*)dataPointer;
+                            *(way+(i*3)) = ', ';
+
+                            dataPointer += 2;
+                        }
+
+                        *(way+waySize*3) = 0x00;
+
+                        //Remounting Graph
+                        g->mountGraph(dataPointer);
+
+                        //Send new graph to all Servos
+                        void *graphSerialized = g->serialize();
+
+                        //Get Buffer Total Size
+                        int bufSize = *(int*)graphSerialized;
+
+                        char *data = new char[bufSize+1];
+                        char *dataPointer = data;
+
+                        //Attach command type to servo
+                        *dataPointer = 'R'; dataPointer++;
+
+                        //Copy Graph
+                        for (int i = 0; i < bufSize; i++) {
+                            *dataPointer = *(char*)graphSerialized;
+                            dataPointer++;
+                        }
+
+                        bufferSize++;
+
+                        //Send Graph to servos
+                        for (int i = 0; i < servos->size(); i++) {
+                            MPI_Send(data, bufSize, MPI_BYTE, i, 0, MPI_COMM_WORLD);
+                        }
+
+                    }
+                }
+            }
 
         }
     }
@@ -82,7 +181,16 @@ int main()
                 char *res = new char[0xFFFFFF];
                 char *resbuf = res;
 
+                //Store servo rank
+                *(unsigned short*)resbuf = (unsigned short)rank;
+                resbuf += 2;
+
+                //Store ResponseType
+                *(char*)resbuf = 'C';
+                resbuf++;
+
                 //4Bytes for buffer total size
+                int *bufferSize = (int*)resbuf;
                 resbuf += 4;
 
                 shortWayResponse *sw = g->shortWay(*(unsigned short*)data+1, *(unsigned short*)data+2);
@@ -102,11 +210,16 @@ int main()
                     resbuf++;
                 }
 
-                *(int*)res = resbuf - res;
+                //Store buffer size
+                *bufferSize = resbuf - res;
                 delete resbuf;
 
                 MPI_Send(res, *(int*)res, MPI_BYTE, 0, 0, MPI_COMM_WORLD);
 
+            }
+            else if (*(char*)data == 'R') {
+                char *graph = (char*)data+1;
+                g->mountGraph(graph);
             }
             else {
                 bool *res = new bool(false);
